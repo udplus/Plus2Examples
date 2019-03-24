ac3fa24cd8c2b6a9ca2a69ca9f567cc8cf449497
YDP <udplus@users.noreply.github.com>
2019-03-24 12:41:06 +0900

    Create Type.GetType.cs

---
+++ Type.GetType.cs
@@ -0,0 +1,4 @@
+ Type T = Type.GetType("ConsoleAppHello.CustomClass");
+            Console.WriteLine(T.FullName);
+            Console.WriteLine(T.Name);
+            Console.WriteLine(T.Namespace);
