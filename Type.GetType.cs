757ba159ba411127e03d9098055b1c544ce6d799
YDP <udplus@gmail.com>
2019-03-24 13:03:46 +0900

    test11

--- Type.GetType.cs
+++ Type.GetType.cs
@@ -1,4 +1,6 @@
- Type T = Type.GetType("ConsoleAppHello.CustomClass");
-            Console.WriteLine(T.FullName);
-            Console.WriteLine(T.Name);
-            Console.WriteLine(T.Namespace);
+Type T = Type.GetType("ConsoleAppHello.CustomClass");
+Console.WriteLine(T.FullName);
+Console.WriteLine(T.Name);
+Console.WriteLine(T.Namespace);
+
+// test
